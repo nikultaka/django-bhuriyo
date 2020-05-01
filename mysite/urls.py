d39586b94e9f47d829e2087c89b175a6bac5dd4d
Dino <Dino>
2020-05-01 13:39:33 +0530

    new functionality

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('about-us', views.about, name="about-us"),
     path('blog', views.blog, name="blog"),
     path('portfolio', views.portfolio, name="portfolio"),
+    path('blog/<int:id>', views.blog_detail, name="blog-detail")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from .models import CMSPage, Portfolio, Testimonial, Blog
+from .models import CMSPage, PortfolioCategory, Portfolio, Testimonial, Blog
 from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -20,14 +20,14 @@ def index(request):
     portfolio = Portfolio.objects.filter()[:5]
     testimonial = Testimonial.objects.filter()[:3]
     blogs = Blog.objects.filter()[:3]
-    context = {'about_us': about_us, 'portfolio': portfolio, 'testimonial': testimonial, 'blogs':blogs}
+    context = {'about_us': about_us, 'portfolio': portfolio, 'testimonial': testimonial, 'blogs': blogs}
     return render(request, 'mysite/index.html', context)
 
 
 def about(request):
 
     about_us = CMSPage.objects.filter(slug='about_us')[:1]
-    context = {"about_us":about_us}
+    context = {"about_us": about_us}
     return render(request, 'mysite/about.html', context)
 
 
@@ -38,10 +38,19 @@ def blog(request):
     return render(request, 'mysite/blog.html', context)
 
 
+def blog_detail(request, id):
+    blog_id = id
+    blogs = Blog.objects.filter(id=blog_id)
+    all_blogs = Blog.objects.filter().exclude(id=blog_id)
+    context = {"blogs": blogs, "all_blogs": all_blogs}
+    return render(request, "mysite/blog_detail.html",context)
+
+
 def portfolio(request):
 
     portfolio = Portfolio.objects.filter()
-    context = {"portfolio": portfolio}
+    portfolio_category = PortfolioCategory.objects.filter()
+    context = {"portfolio": portfolio, "portfolioCategory": portfolio_category}
     return render(request, "mysite/portfolio.html", context)
 
 
@@ -52,9 +61,11 @@ def contact(request):
     email = request.POST.get('email')
     message = request.POST.get('message')
 
+    portal_contact_email = preferences.MyPreferences.portal_contact_email
+
     body = "Hi, we got new inquiry from, name : "+name+', Message : '+message
 
-    email = EmailMessage('New Inquiry', body, to=['nikultaka@gmail.com'])
+    email = EmailMessage('New Inquiry', body, to=[portal_contact_email])
     email.send()
 
     data = {}
